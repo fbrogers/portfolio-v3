cf6538680017c3379ab25705c042fb6030386a74
Jordan Rogers <fbrogers@gmail.com>
2020-10-13 21:05:16 -0400

    first video attempt

--- assets/scripts.js
+++
@@ -1,7 +0,0 @@
-$(function(){ 
-    //open external links and PDF files in new tabs
-    $("a.external, a[href$='.pdf']").click(function(e){
-        window.open(this.href);
-        e.preventDefault();
-    });
-});
